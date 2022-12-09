465a406b0338f83cfb6a999d6e4e49b77b6634de
i19yanagi <i19yanagi@gmail.com>
2022-12-10 01:09:37 +0900

    :tada: setup postgres

---
+++ script/01_initialize.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE example;
+
+\c example
+
+CREATE ROLE hoge WITH LOGIN PASSWORD 'passw0rd';
---
+++ script/02_create_table.sql
@@ -0,0 +1,21 @@
+\c example
+
+CREATE TABLE menu (
+    id serial not null primary key
+    , menu varchar(16) not null
+    , price int not null
+    , stock int not null
+);
+
+CREATE TABLE customer (
+    id serial not null primary key
+    , menu varchar(256) not null
+    , price int not null
+    , ordered_at timestamp not null
+);
+
+GRANT ALL PRIVILEGES ON menu TO hoge;
+
+INSERT INTO menu (menu, price, stock) VALUES ('カツカレー', 360, 100);
+INSERT INTO menu (menu, price, stock) VALUES ('カツ丼', 360, 100);
+INSERT INTO menu (menu, price, stock) VALUES ('親子丼', 360, 100);

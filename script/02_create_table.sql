335f3129b0faf81675d901f1902adb5512bf6264
i19yanagi <i19yanagi@gmail.com>
2023-03-06 11:16:31 +0900

    :sparkles: feat: create view

--- script/02_create_table.sql
+++ script/02_create_table.sql
@@ -16,7 +16,7 @@ CREATE TABLE products (
 );
 
 CREATE TABLE orders (
-    id serial PRIMARY KEY,
+    id uuid PRIMARY KEY,
     user_id int,
     total int NOT NULL,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -25,16 +25,37 @@ CREATE TABLE orders (
 
 CREATE TABLE order_items (
     id serial PRIMARY KEY,
-    order_id int NOT NULL,
-    items int NOT NULL,
-    quantity int NOT NULL
+    order_id uuid NOT NULL,
+    product_id int NOT NULL,
+    quantity int NOT NULL,
+    UNIQUE (order_id, product_id)
 );
 
 ALTER TABLE orders ADD FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE order_items ADD FOREIGN KEY (order_id) REFERENCES orders (id);
 
-ALTER TABLE order_items ADD FOREIGN KEY (items) REFERENCES products (id);
+ALTER TABLE order_items ADD FOREIGN KEY (product_id) REFERENCES products (id);
+
+CREATE TYPE item AS (name VARCHAR(256), price INT, quantity INT);
+
+CREATE OR REPLACE VIEW order_view AS
+    SELECT
+        o.id,
+        o.user_id,
+        o.created_at,
+        o.total,
+        o.status,
+        ARRAY_AGG(ROW(p.name, p.price, i.quantity)::item) AS items
+    FROM
+        orders AS o
+            INNER JOIN
+        order_items AS i
+        ON i.order_id = o.id
+            INNER JOIN
+        products AS p
+        ON p.id = i.product_id
+    GROUP BY o.id;
 
 INSERT INTO products (name, price, stock)
     VALUES
